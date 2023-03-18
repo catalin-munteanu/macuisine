433c3562eba0f0c3d07b7b9eee2781bd6532cda6
Catalín Munteanu <catalinapampin@gmail.com>
2023-03-18 00:05:30 -0300

    Avance en menú hamburguesa

--- js/main.js
+++ js/main.js
@@ -1,9 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const burgerButtons = document.getElementsByClassName('burger-menu-toggler');
-    for (let i = 0; i < burgerButtons.length; i++) {
-        const element = burgerButtons[i];
-        burgerButtons[i].addEventListener('click', function() {
-            document.body.classList.toggle('burger-menu-active')
-        })
+
+    const burgerMenuBtn = document.querySelector('#burger-menu-toggler');
+    const menuItems = document.querySelectorAll('.menuItems');
+    
+    function navResponsive() {
+    
+        burgerMenuBtn.addEventListener( 'click', function() {
+            document.body.classList.toggle('burger-menu-active');
+    
+            });
+    
+        menuItems.forEach(function(menuItem) {
+            menuItem.addEventListener('click', function () {
+                document.body.classList.remove('burger-menu-active');
+                let currentItem = document.querySelector('.active');
+                currentItem.classList.remove('active');
+                this.classList.add('active');
+            })
+        });
     }
-} );
+    
+    navResponsive();
+    
+    } )
+    
+    let pathname = window.location.pathname;
+    
+    switch (pathname) {
+        case "/index.html":
+            document.querySelector("#home").classList.add('active');
+        break;
+        case "/bio.html":
+        document.querySelector("#about").classList.add('active');
+        break;
+        case "/contacto.html":
+            document.querySelector("#projects").classList.add('active');
+        break;
+        case "/musica.html":
+        document.querySelector("#contact").classList.add('active');
+        break;
+        case "/participaciones.html":
+            document.querySelector("#contact").classList.add('active');
+            break;
+        case "/videos.html":
+            document.querySelector("#contact").classList.add('active');
+        break;
+        case "/vivo.html":
+            document.querySelector("#contact").classList.add('active');
+        break;        
+    } 
